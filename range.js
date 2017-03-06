7b652babdb6fbf55905a07cba53a1ea82a1a5065
kantuni <henrikh.kantuni@gmail.com>
2017-03-06 14:09:04 -0500

    Close #16

--- range.js
+++ range.js
@@ -5,175 +5,34 @@
 
 'use strict';
 
-let Range = (() => {
-  // BTDT: old value is used in "on value change" callback
-  let oldValue, value, min, max, step, breakpoints;
-  let wrapperWidth, stepWidth;
-  let wrapper, fill, handle, element, outputList = [];
-  // let onInitCallback, onSlideCallback, onValueChangeCallback, onSlideEndCallback;
-
-
+class Range {
   /**
-   * Handle "mouse down" events.
-   * @param event
-   * @private
+   * Initialize a Range1 element.
+   * @param input - <input type="range"> element
+   * @param output - output element
+   * @param callback - on init callback
    */
-  function _mouseDown(event) {
-    // disable selection (Safari)
-    event.preventDefault();
+  constructor(input, output, callback) {
+    this.input = input;
+    this.output = output;
 
-    // add event listeners to "mouse move" and "mouse up" events
-    // BTDT: attach events to the document, not the element
-    document.addEventListener('mousemove', _mouseMove);
-    document.addEventListener('mouseup', _mouseUp);
-
-    // set old value
-    oldValue = value;
-
-    // get the active range
-    wrapper = event.target.parentNode;
-    fill = wrapper.querySelector('.range-fill');
-    handle = event.target;
-    element = wrapper.querySelector('input[type="range"]');
-    outputList = document.Range.get(wrapper).outputList;
-
-    _initialCalculations();
-
-    // disable selection
-    return false;
-  }
-
-  /**
-   * Handle "mouse move" events.
-   * @param event
-   * @private
-   */
-  function _mouseMove(event) {
-    // disable selection
-    event.preventDefault();
-
-    let valueBeforeSlide = value;
-    let newWidth = event.pageX - wrapper.getBoundingClientRect().left;
-
-    // stay in wrapper bounds
-    if (newWidth > wrapperWidth) {
-      newWidth = wrapperWidth;
-    } else if (newWidth < 0) {
-      newWidth = 0;
+    if (!this._isValid(this.input)) {
+      return;
     }
 
-    value = _calculateValue(newWidth);
+    // create necessary DOM elements
+    this._createDOMElements();
 
-    if (valueBeforeSlide !== value) {
-      // update value
-      _updateValue(value);
+    // calculate the value, breakpoints, etc.
+    this._initialCalculations();
+
+    // attach events on init
+    this._attachInitEvents();
+
+    // "on init" callback call
+    if (this.onInitCallback = callback) {
+      this.onInitCallback();
     }
-
-    // "on slide" callback call
-    let onSlideCallback = document.Range.get(wrapper).onSlideCallback;
-    if (onSlideCallback) {
-      onSlideCallback();
-    }
-  }
-
-  /**
-   * Handle "mouse up" events.
-   * @param event
-   * @private
-   */
-  function _mouseUp(event) {
-    // remove "mouse move" and "mouse up" events
-    document.removeEventListener('mousemove', _mouseMove);
-    document.removeEventListener('mouseup', _mouseUp);
-
-    // "on value change" callback call
-    let onValueChangeCallback = document.Range.get(wrapper).onValueChangeCallback;
-    if (onValueChangeCallback && oldValue !== value) {
-      onValueChangeCallback();
-    }
-
-    // "on slide end" callback call
-    let onSlideEndCallback = document.Range.get(wrapper).onSlideEndCallback;
-    if (onSlideEndCallback) {
-      onSlideEndCallback();
-    }
-  }
-
-  /**
-   * Create DOM elements for wrapper, fill, and handle.
-   * @private
-   */
-  function _createDOMElements() {
-    // create a wrapper
-    wrapper = document.createElement('div');
-    wrapper.classList.add('range');
-
-    // create a "range-fill"
-    fill = document.createElement('span');
-    fill.classList.add('range-fill');
-
-    // create a handle
-    handle = document.createElement('span');
-    handle.classList.add('range-handle');
-
-    // add a wrapper element to the DOM
-    element.parentNode.insertBefore(wrapper, element);
-
-    // append a "range-fill" to the wrapper
-    wrapper.appendChild(fill);
-
-    // append a handle to the wrapper
-    wrapper.appendChild(handle);
-
-    // append an element to the wrapper
-    wrapper.appendChild(element);
-
-    // hide input
-    element.style.display = 'none';
-  }
-
-  /**
-   * Attach events on init.
-   * @private
-   */
-  function _attachInitEvents() {
-    // set a new value on click
-    wrapper.addEventListener('click', (event) => {
-      if (event.target == wrapper || event.target == fill) {
-        value = _calculateValue(event.offsetX);
-
-        // update value
-        _updateValue(value);
-      }
-    });
-
-    // disable default "drag start" event handler
-    handle.addEventListener('dragstart', () => {
-      return false;
-    });
-
-    // add a custom "mouse down" event handler
-    handle.addEventListener('mousedown', _mouseDown);
-  }
-
-  /**
-   * Do initial calculations.
-   * @private
-   */
-  function _initialCalculations() {
-    min = Number(element.getAttribute('min'));
-    max = Number(element.getAttribute('max'));
-
-    // get wrapper width
-    wrapperWidth = parseInt(window.getComputedStyle(wrapper).width, 10);
-
-    step = Number(element.getAttribute('step'));
-    breakpoints = Math.ceil((max - min + 1) / step);
-    stepWidth = wrapperWidth / (breakpoints - 1);
-
-    // default value = min
-    value = element.getAttribute('value') ? Number(element.getAttribute('value')) : min;
-    setValue(value);
   }
 
   /**
@@ -182,7 +41,7 @@ let Range = (() => {
    * @return {boolean}
    * @private
    */
-  function _isValid(element) {
+  _isValid(element) {
     let valid = true;
 
     // min is required
@@ -237,117 +96,63 @@ let Range = (() => {
   }
 
   /**
-   * Calculate a new value based on a new width.
-   * @param newWidth - the new width of the "range-fill"
-   * @return the new value
+   * Create DOM elements for wrapper, fill, and handle.
    * @private
    */
-  function _calculateValue(newWidth) {
-    // BTDT: set start width = -1, not 0 to avoid additional if statement
-    let newValue, startWidth = -1, endWidth = stepWidth / 2;
+  _createDOMElements() {
+    // create a wrapper
+    this.wrapper = document.createElement('div');
+    this.wrapper.classList.add('range');
 
-    // loop over breakpoints to find out where the value lies
-    for (let i = 0; i < breakpoints; ++i) {
-      if (newWidth > startWidth && newWidth <= endWidth) {
-        newValue = min + i * step;
-        break;
-      }
+    // create a "range-fill"
+    this.fill = document.createElement('span');
+    this.fill.classList.add('range-fill');
 
-      startWidth = endWidth;
-      endWidth = endWidth + stepWidth;
-    }
+    // create a handle
+    this.handle = document.createElement('span');
+    this.handle.classList.add('range-handle');
 
-    // discontinuous drag effect
-    // BTDT: remove this to allow smooth effect
-    newWidth = _calculateWidth(newValue);
+    // add a wrapper element to the DOM
+    this.input.parentNode.insertBefore(this.wrapper, this.input);
 
-    // update width
-    fill.style.width = newWidth + 'px';
+    // append a "range-fill" to the wrapper
+    this.wrapper.appendChild(this.fill);
 
-    return newValue;
+    // append a handle to the wrapper
+    this.wrapper.appendChild(this.handle);
+
+    // append an element to the wrapper
+    this.wrapper.appendChild(this.input);
+
+    // hide input
+    this.input.style.display = 'none';
   }
 
   /**
-   * Calculate a new fill width based on a new value.
-   * @param newValue - the new value
-   * @return {*} the new width
+   * Do initial calculations.
    * @private
    */
-  function _calculateWidth(newValue) {
-    return (Math.abs(newValue - min) / step) * stepWidth;
+  _initialCalculations() {
+    this.min = Number(this.input.getAttribute('min'));
+    this.max = Number(this.input.getAttribute('max'));
+
+    // get wrapper width
+    this.wrapperWidth = parseInt(window.getComputedStyle(this.wrapper).width, 10);
+
+    this.step = Number(this.input.getAttribute('step'));
+    this.breakpoints = Math.ceil((this.max - this.min + 1) / this.step);
+    this.stepWidth = this.wrapperWidth / (this.breakpoints - 1);
+
+    // default value = min
+    this.value = this.input.getAttribute('value') ? Number(this.input.getAttribute('value')) : this.min;
+    this.setValue(this.value);
   }
-
-  /**
-   * Update a range value.
-   * @param newValue
-   * @private
-   */
-  function _updateValue(newValue) {
-    if (document.Range && document.Range.has(wrapper)) {
-      document.Range.get(wrapper).value = value;
-    }
-    
-    // update input value
-    element.setAttribute('value', newValue);
-
-    // update output list values
-    if (outputList) {
-      for (let i = 0; i < outputList.length; ++i) {
-        outputList[i].textContent = newValue;
-      }
-    }
-  }
-
-
-  /**
-   * Initialize a range element.
-   * @param input - <input type="range"> element
-   * @param output - the list of output elements
-   * @param callback - on init callback
-   * @return {*} a Range element
-   */
-  let init = (input, output = [], callback) => {
-    element = input;
-    outputList = output;
-
-    if (!_isValid(element)) {
-      return;
-    }
-
-    // create necessary DOM elements
-    _createDOMElements();
-
-    // calculate the value, breakpoints, etc.
-    _initialCalculations();
-
-    // attach events on init
-    _attachInitEvents();
-
-    // multiple range instances support
-    if (!document.Range) {
-      document.Range = new Map();
-    }
-
-    document.Range.set(wrapper, {
-      value: value,
-      outputList: outputList
-    });
-
-    // "on init" callback call
-    let onInitCallback;
-    if (onInitCallback = callback) {
-      onInitCallback();
-    }
-
-    // allow chaining
-    return Range;
-  };
 
   /**
    * Set a new range value.
    * @param newValue - the new value
    */
-  let setValue = (newValue) => {
+  setValue(newValue) {
     // the new value must be a number
     if (isNaN(Number(newValue))) {
       console.warn("The new value must be a number.");
@@ -357,68 +162,210 @@ let Range = (() => {
     newValue = Number(newValue);
 
     // out of bounds
-    if (newValue < min || newValue > max) {
+    if (newValue < this.min || newValue > this.max) {
       console.warn("The new value is out of bounds.");
       return;
     }
 
-    if ((newValue - min) % step !== 0) {
+    if ((newValue - this.min) % this.step !== 0) {
       console.warn("The new value is not available with these range parameters.");
       return;
     }
 
     // set width
-    fill.style.width = _calculateWidth(newValue) + 'px';
+    this.fill.style.width = this._calculateWidth(newValue) + 'px';
 
     // set value
-    value = newValue;
+    this.value = newValue;
 
     // update value
-    _updateValue(newValue);
+    this._updateValue(newValue);
   };
+
+  /**
+   * Calculate a new fill width based on a new value.
+   * @param newValue - the new value
+   * @return {*} the new width
+   * @private
+   */
+  _calculateWidth(newValue) {
+    return (Math.abs(newValue - this.min) / this.step) * this.stepWidth;
+  }
+
+  /**
+   * Update a range value.
+   * @param newValue
+   * @private
+   */
+  _updateValue(newValue) {
+    // update input value
+    this.input.setAttribute('value', newValue);
+
+    // update output list values
+    if (this.output) {
+      this.output.textContent = newValue;
+    }
+  }
+
+  /**
+   * Attach events on init.
+   * @private
+   */
+  _attachInitEvents() {
+    // set a new value on click
+    this.wrapper.addEventListener('click', (event) => {
+      if (event.target == this.wrapper || event.target == this.fill) {
+        this.value = this._calculateValue(event.offsetX);
+
+        // update value
+        this._updateValue(this.value);
+      }
+    });
+
+    // disable default "drag start" event handler
+    this.handle.addEventListener('dragstart', () => false);
+
+    // add a custom "mouse down" event handler
+    this._mouseDown = this._mouseDown.bind(this);
+    this.handle.addEventListener('mousedown', this._mouseDown);
+  }
+
+  /**
+   * Handle "mouse down" events.
+   * @param event
+   * @private
+   */
+  _mouseDown(event) {
+    // disable selection (Safari)
+    event.preventDefault();
+
+    // add event listeners to "mouse move" and "mouse up" events
+    // BTDT: attach events to the document, not the element
+    this._mouseMove = this._mouseMove.bind(this);
+    this._mouseUp = this._mouseUp.bind(this);
+    document.addEventListener('mousemove', this._mouseMove);
+    document.addEventListener('mouseup', this._mouseUp);
+
+    // set old value
+    this.oldValue = this.value;
+    
+    // disable selection
+    return false;
+  }
+
+  /**
+   * Handle "mouse move" events.
+   * @param event
+   * @private
+   */
+  _mouseMove(event) {
+    // disable selection
+    event.preventDefault();
+
+    let valueBeforeSlide = this.value;
+    let newWidth = event.pageX - this.wrapper.getBoundingClientRect().left;
+
+    // stay in wrapper bounds
+    if (newWidth > this.wrapperWidth) {
+      newWidth = this.wrapperWidth;
+    } else if (newWidth < 0) {
+      newWidth = 0;
+    }
+
+    this.value = this._calculateValue(newWidth);
+
+    if (valueBeforeSlide !== this.value) {
+      // update value
+      this._updateValue(this.value);
+    }
+
+    // "on slide" callback call
+    if (this.onSlideCallback) {
+      this.onSlideCallback();
+    }
+  }
+
+  /**
+   * Calculate a new value based on a new width.
+   * @param newWidth - the new width of the "range-fill"
+   * @return the new value
+   * @private
+   */
+  _calculateValue(newWidth) {
+    // BTDT: set start width = -1, not 0 to avoid additional if statement
+    let newValue, startWidth = -1, endWidth = this.stepWidth / 2;
+
+    // loop over breakpoints to find out where the value lies
+    for (let i = 0; i < this.breakpoints; ++i) {
+      if (newWidth > startWidth && newWidth <= endWidth) {
+        newValue = this.min + i * this.step;
+        break;
+      }
+
+      startWidth = endWidth;
+      endWidth = endWidth + this.stepWidth;
+    }
+
+    // discontinuous drag effect
+    // BTDT: remove this to allow smooth effect
+    newWidth = this._calculateWidth(newValue);
+
+    // update width
+    this.fill.style.width = newWidth + 'px';
+
+    return newValue;
+  }
+
+  /**
+   * Handle "mouse up" events.
+   * @param event
+   * @private
+   */
+  _mouseUp(event) {
+    // remove "mouse move" and "mouse up" events
+    document.removeEventListener('mousemove', this._mouseMove);
+    document.removeEventListener('mouseup', this._mouseUp);
+
+    // "on value change" callback call
+    if (this.onValueChangeCallback && this.oldValue !== this.value) {
+      this.onValueChangeCallback();
+    }
+
+    // "on slide end" callback call
+    if (this.onSlideEndCallback) {
+      this.onSlideEndCallback();
+    }
+  }
 
   /**
    * Get a range value.
    * @return the range value
    */
-  let getValue = () => {
-    return document.Range.get(wrapper).value;
+  getValue() {
+    return this.value;
   };
 
   /**
    * "On slide" event handler.
    * @param callback
    */
-  let onSlide = (callback) => {
-    // onSlideCallback = callback;
-    document.Range.get(wrapper).onSlideCallback = callback;
+  onSlide(callback) {
+    this.onSlideCallback = callback;
   };
 
   /**
    * "On value change" event handler.
    * @param callback
    */
-  let onValueChange = (callback) => {
-    // onValueChangeCallback = callback;
-    document.Range.get(wrapper).onValueChangeCallback = callback;
+  onValueChange(callback) {
+    this.onValueChangeCallback = callback;
   };
 
   /**
    * "On slide end" event handler.
    * @param callback
    */
-  let onSlideEnd = (callback) => {
-    // onSlideEndCallback = callback;
-    document.Range.get(wrapper).onSlideEndCallback = callback;
+  onSlideEnd(callback) {
+    this.onSlideEndCallback = callback;
   };
-
-
-  return {
-    init: init,
-    setValue: setValue,
-    getValue: getValue,
-    onSlide: onSlide,
-    onValueChange: onValueChange,
-    onSlideEnd: onSlideEnd
-  };
-})();
+}
